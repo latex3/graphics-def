aabe80116c5dca2ac5c59200618e1851739e2550
David Carlisle <d.p.carlisle@gmail.com>
2016-01-20 12:53:37 +0000

    replace mkctan script with l3build version

---
+++ build.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env texlua
+
+-- Build script for "luatex-def" files
+
+-- Identify the bundle and module
+bundle = ""
+module = "luatex-def"
+
+-- Ignore ifpdf stuff
+sourcefiles =
+  {
+    "luatex.def",
+  }
+typesetfiles =
+  {
+  }
+
+-- No automated tests for this bundle
+testfildir = ""
+
+-- Find and run the build system
+kpse.set_program_name ("kpsewhich")
+dofile (kpse.lookup ("l3build.lua"))
