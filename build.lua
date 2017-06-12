8f182e2f06d84225af85e48aec69785cd6119714
Joseph Wright <joseph.wright@morningstar2.co.uk>
2017-06-12 19:54:04 +0100

    Tidy up build script

--- build.lua
+++ build.lua
@@ -11,13 +11,6 @@ installfiles = {"*.def"}
 sourcefiles  = {"*.def"}
 typesetfiles = {}
 
--- TDS info (not required at present)
---tdsroot = "luatex"
-
--- No automated tests for this bundle (yet)
-testfiledir = ""
-
-
 -- Find and run the build system
-kpse.set_program_name ("kpsewhich")
-dofile (kpse.lookup ("l3build.lua"))
+kpse.set_program_name("kpsewhich")
+dofile(kpse.lookup("l3build.lua"))
