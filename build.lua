cec2feac6024d8d24d6f1404843df729525dc5f8
David Carlisle <d.p.carlisle@gmail.com>
2020-03-24 10:17:33 +0000

    specialformats switch for luahbtex

--- build.lua
+++ build.lua
@@ -11,6 +11,25 @@ installfiles = {"*.def"}
 sourcefiles  = {"*.def"}
 typesetfiles = {}
 
+if string.find(status.banner,"2019") then
+  print("TL2019")
+  TL2019bool=true
+else
+  -- tl2020
+  print("TL2020 or later")
+
+ specialformats={}
+  specialformats["latex"] = specialformats["latex"] or
+   {
+    luatex     = {binary="luahbtex",format = "lualatex"},
+   }
+  specialformats["latex-dev"] = specialformats["latex-dev"] or
+   {
+    luatex = {binary="luahbtex",format = "lualatex-dev"}
+   }
+end
+
+
 -- Find and run the build system
 kpse.set_program_name ("kpsewhich")
 if not release_date then
