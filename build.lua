79cc11cb0db907bb7deeecae03680c76ff0ab3a1
Joseph Wright <joseph.wright@morningstar2.co.uk>
2021-01-07 14:42:12 +0000

    Set up for development branch releases

--- build.lua
+++ build.lua
@@ -22,6 +22,31 @@ specialformats["latex-dev"] = specialformats["latex-dev"] or
      luatex = {binary="luahbtex",format = "lualatex-dev"}
   }
 
+-- Allow for 'dev' release
+--
+-- See stackoverflow.com/a/12142066/212001
+-- Also luaotfload build.lua: getting the Travis-CI version right is 'fun'
+do
+  local tag = os.getenv'TRAVIS_TAG'
+  if tag and tag ~= "" then
+    master_branch = not string.match(tag, '^dev-')
+  else
+    local branch = os.getenv'TRAVIS_BRANCH'
+    if not branch then
+      local f = io.popen'git rev-parse --abbrev-ref HEAD'
+      branch = f:read'*a':sub(1,-2)
+      assert(f:close())
+    end
+    master_branch = string.match(branch, '^master')
+  end
+  if not master_branch then
+    tdsroot = "latex-dev"
+    print("Creating/installing dev-version in " .. tdsroot)
+    ctanpkg = "graphics-def-dev"
+    ctanzip = ctanpkg
+  end
+end
+
 -- Find and run the build system
 kpse.set_program_name ("kpsewhich")
 if not release_date then
