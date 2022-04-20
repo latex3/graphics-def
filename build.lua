934210b4bb6d03e73fbbad0b1fcad1b192cdb292
Joseph Wright <joseph.wright@morningstar2.co.uk>
2022-04-20 07:13:50 +0100

    Drop 'dev' release support
    This was out of date anyway.

--- build.lua
+++ build.lua
@@ -21,28 +21,3 @@ specialformats["latex-dev"] = specialformats["latex-dev"] or
   {
      luatex = {binary="luahbtex",format = "lualatex-dev"}
   }
-
--- Allow for 'dev' release
---
--- See stackoverflow.com/a/12142066/212001
--- Also luaotfload build.lua: getting the Travis-CI version right is 'fun'
-do
-  local tag = os.getenv'TRAVIS_TAG'
-  if tag and tag ~= "" then
-    main_branch = not string.match(tag, '^dev-')
-  else
-    local branch = os.getenv'TRAVIS_BRANCH'
-    if not branch then
-      local f = io.popen'git rev-parse --abbrev-ref HEAD'
-      branch = f:read'*a':sub(1,-2)
-      assert(f:close())
-    end
-    main_branch = string.match(branch, '^main')
-  end
-  if not main_branch then
-    tdsroot = "latex-dev"
-    print("Creating/installing dev-version in " .. tdsroot)
-    ctanpkg = "graphics-def-dev"
-    ctanzip = ctanpkg
-  end
-end
